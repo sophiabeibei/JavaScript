ae72930027852fd332b84738327319fb7f9f8b88
sophiabeibei <duguyiren681@163.com>
2017-07-30 12:34:00 +0800

    内置类/构造函数/工厂模式/单利模式

---
+++ 01_20170711TeacherZhou/FirstWeek/20170730/js/1.1.js
@@ -0,0 +1,47 @@
+// var name = "AA";
+// var age = "84";
+// var sex = "未知";
+// var friend = "BB";
+//
+// var name = "BB";
+// var tel = "13800001111";
+// var age = "85";
+// var sex = "男";
+//-------------------------------------------
+
+//诞生了一种数据类型: 对象数据类型;
+//组一:
+var person1 = {
+    name : "AA",
+    age : "84"
+};
+
+//组一:
+var person2 = {
+    name : "BB",
+    age : "85"
+};
+console.log(person1.name);
+console.log(person2.age);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
