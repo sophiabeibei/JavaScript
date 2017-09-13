73c5198c06e83177ce68884614285a97d6bdcbe2
sophiabeibei <duguyiren681@163.com>
2017-09-13 19:25:04 +0800

    resume简历-H5场景创意简历

--- 01_201708TeacherZhou/EighthWeek/20170913/RESUME/js/resume.js
+++ 01_201708TeacherZhou/EighthWeek/20170913/RESUME/js/resume.js
@@ -104,6 +104,86 @@ let loadingRender = (function () {
 /*--phone--*/
 //以后phone做什么事情就写到这里来;
 let phoneRender = (function () {
+    let $phone=$(".phone"),
+        $listen=$phone.find(".listen"),
+        $listenTouch=$listen.find(".touch"),
+        $detail=$phone.find(".detail"),
+        $detailTouch=$detail.find(".touch"),
+        $time=$phone.find("span");
+
+    let bellAudio=$("#bellAudio")[0],
+        sayAudio=$("#sayAudio")[0];
+
+    function listenTouch() {
+        $listenTouch.singleTap(function () {//singleTap: 单击
+            bellAudio.pause();
+            $(bellAudio).remove();
+
+            $listen.remove();
+            $detail.css("transform","translateY(0)");
+
+            //->让面板出来之后,
+            $detail.on("webkitTransitionEnd",function () {
+                //->transition动画结束的时候(JS中控制css3,只写一套加前缀的即可);
+                //->让time显示
+                $time.css("display","block");
+
+                //->播放say音乐;
+                sayAudio.play();
+                watchTime();//监听
+            });
+        });
+    }
+
+    let watchTimer = null;
+
+    function watchTime() {
+        watchTimer = setInterval(()=>{
+            //->this: 上级作用域中的this是谁,箭头函数中的this就是谁;
+            var curTime = sayAudio.currentTime,
+                durTime = sayAudio.duration;
+            if(curTime>=durTime){
+                //->已经播放的时间大于等于总时间,说明播放完成,播放完成之后执行以下
+                message();
+                return;
+            }
+            var minute=Math.floor(curTime/60),
+                second=Math.ceil(curTime-minute*60);
+            minute<10?minute="0"+minute:null;
+            second<10?second="0"+second:null;
+            $time.html(minute+":"+second);
+        },1000);
+    }
+
+    function message() {
+        clearInterval(watchTime);
+        sayAudio.pause();
+        $(sayAudio).remove();
+        $phone.remove();
+        messageRender.init();
+    }
+
+    return {
+        init: function () {
+            $phone.css("display","block");
+
+            //->paly bell
+            bellAudio.play();
+
+            //->listen touch
+            listenTouch();
+
+            //->detail touch
+            $detailTouch.singleTap(message);//singleTap: 单击;
+
+        }
+    }
+})();
+
+
+/*--message--*/
+let messageRender = (function () {
+
     return {
         init: function () {
 
@@ -111,8 +191,7 @@ let phoneRender = (function () {
     }
 })();
 
-
-loadingRender.init();
+phoneRender.init();
 
 
 
