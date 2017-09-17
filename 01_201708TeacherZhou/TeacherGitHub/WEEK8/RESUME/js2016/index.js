dbaf2bb0af7cb9a6d638bedd2add27bb36fff56f
sophiabeibei <duguyiren681@163.com>
2017-09-17 09:40:09 +0800

    resume/iscroll/sina移动端案例

--- 01_201708TeacherZhou/TeacherGitHub/WEEK8/RESUME/js2016/index.js
+++ 01_201708TeacherZhou/TeacherGitHub/WEEK8/RESUME/js2016/index.js
@@ -1,56 +1,14 @@
-/*--LOADING--*/
-let loadingRender = (function () {
-    //->需要预先加载的所有图片
-    let imgList = ['img/icon.png',
-        'img/music.svg',
-        'img/zf_concatAddress.png',
-        'img/zf_concatInfo.png',
-        'img/zf_concatPhone.png',
-        'img/zf_course.png',
-        'img/zf_course1.png',
-        'img/zf_course2.png',
-        'img/zf_course3.png',
-        'img/zf_course4.png',
-        'img/zf_course5.png',
-        'img/zf_course6.png',
-        'img/zf_cube1.png',
-        'img/zf_cube2.png',
-        'img/zf_cube3.png',
-        'img/zf_cube4.png',
-        'img/zf_cube5.png',
-        'img/zf_cube6.png',
-        'img/zf_cubeBg.jpg',
-        'img/zf_cubeTip.png',
-        'img/zf_emploment.png',
-        'img/zf_messageArrow1.png',
-        'img/zf_messageArrow2.png',
-        'img/zf_messageChat.png',
-        'img/zf_messageKeyboard.png',
-        'img/zf_messageLogo.png',
-        'img/zf_messageStudent.png',
-        'img/zf_outline.png',
-        'img/zf_phoneBg.jpg',
-        'img/zf_phoneDetail.png',
-        'img/zf_phoneListen.png',
-        'img/zf_phoneLogo.png',
-        'img/zf_return.png',
-        'img/zf_style1.jpg',
-        'img/zf_style2.jpg',
-        'img/zf_style3.jpg',
-        'img/zf_styleTip1.png',
-        'img/zf_styleTip2.png',
-        'img/zf_teacher1.png',
-        'img/zf_teacher2.png',
-        'img/zf_teacher3.jpg',
-        'img/zf_teacher4.png',
-        'img/zf_teacher5.png',
-        'img/zf_teacher6.png',
-        'img/zf_teacherTip.png'];
+'use strict';
 
-    let $loading = $('.loading'),
+/*--LOADING--*/
+var loadingRender = function () {
+    //->需要预先加载的所有图片
+    var imgList = ['img/icon.png', 'img/music.svg', 'img/zf_concatAddress.png', 'img/zf_concatInfo.png', 'img/zf_concatPhone.png', 'img/zf_course.png', 'img/zf_course1.png', 'img/zf_course2.png', 'img/zf_course3.png', 'img/zf_course4.png', 'img/zf_course5.png', 'img/zf_course6.png', 'img/zf_cube1.png', 'img/zf_cube2.png', 'img/zf_cube3.png', 'img/zf_cube4.png', 'img/zf_cube5.png', 'img/zf_cube6.png', 'img/zf_cubeBg.jpg', 'img/zf_cubeTip.png', 'img/zf_emploment.png', 'img/zf_messageArrow1.png', 'img/zf_messageArrow2.png', 'img/zf_messageChat.png', 'img/zf_messageKeyboard.png', 'img/zf_messageLogo.png', 'img/zf_messageStudent.png', 'img/zf_outline.png', 'img/zf_phoneBg.jpg', 'img/zf_phoneDetail.png', 'img/zf_phoneListen.png', 'img/zf_phoneLogo.png', 'img/zf_return.png', 'img/zf_style1.jpg', 'img/zf_style2.jpg', 'img/zf_style3.jpg', 'img/zf_styleTip1.png', 'img/zf_styleTip2.png', 'img/zf_teacher1.png', 'img/zf_teacher2.png', 'img/zf_teacher3.jpg', 'img/zf_teacher4.png', 'img/zf_teacher5.png', 'img/zf_teacher6.png', 'img/zf_teacherTip.png'];
+
+    var $loading = $('.loading'),
         $already = $loading.find('.already');
 
-    let n = 0,
+    var n = 0,
         m = imgList.length,
         timer = null,
         isEnter = false;
@@ -58,12 +16,12 @@ let loadingRender = (function () {
     //->加载图片,计算加载量
     function loadImg() {
         $.each(imgList, function (index, item) {
-            let oImg = new Image;
+            var oImg = new Image();
             oImg.src = item;
             oImg.onload = function () {
-                $already.css('width', (++n) / m * 100 + '%');
+                $already.css('width', ++n / m * 100 + '%');
                 if (n >= m) {
-                    setTimeout(()=> {
+                    setTimeout(function () {
                         //->加载完成:LOADING消失,PHONE操作(延迟2S)
                         if (isEnter) return;
                         isEnter = true;
@@ -72,40 +30,40 @@ let loadingRender = (function () {
                         phoneRender.init();
                     }, 2000);
                 }
-            }
+            };
         });
     }
 
     return {
-        init: function () {
+        init: function init() {
             $loading.css('display', 'block');
             loadImg();
             //->给一个限定的时间(超过10S,如果有个别图片无法加载,我们也直接进入到下一个操作了)
-            timer = setTimeout(()=> {
+            timer = setTimeout(function () {
                 if (n < m) {
                     if (isEnter) return;
                     isEnter = true;
                     $already.css('width', '100%');
-                    setTimeout(()=> {
+                    setTimeout(function () {
                         $loading.remove();
                         phoneRender.init();
                     }, 2000);
                 }
             }, 10000);
         }
-    }
-})();
+    };
+}();
 
 /*--PHONE--*/
-let phoneRender = (function () {
-    let $phone = $('.phone'),
+var phoneRender = function () {
+    var $phone = $('.phone'),
         $listen = $phone.find('.listen'),
         $listenTouch = $listen.find('.touch'),
         $detail = $phone.find('.detail'),
         $detailTouch = $detail.find('.touch'),
         $time = $phone.find('span');
 
-    let bellAudio = $('#bellAudio')[0],
+    var bellAudio = $('#bellAudio')[0],
         sayAudio = $('#sayAudio')[0];
 
     function listenTouch() {
@@ -114,21 +72,20 @@ let phoneRender = (function () {
             $(bellAudio).remove();
 
             $listen.remove();
-            $detail.css('transform', 'translateY(0)')
-                .on('webkitTransitionEnd', function () {
-                    //->TRANSITION动画结束
-                    //JS中控制CSS3属性,只写一套加前缀的即可
-                    $time.css('display', 'block');
-                    sayAudio.play();
-                    watchTime();
-                });
+            $detail.css('transform', 'translateY(0)').on('webkitTransitionEnd', function () {
+                //->TRANSITION动画结束
+                //JS中控制CSS3属性,只写一套加前缀的即可
+                $time.css('display', 'block');
+                sayAudio.play();
+                watchTime();
+            });
         });
     }
 
-    let watchTimer = null;
+    var watchTimer = null;
 
     function watchTime() {
-        watchTimer = setInterval(()=> {
+        watchTimer = setInterval(function () {
             var curTime = sayAudio.currentTime,
                 durTime = sayAudio.duration;
             if (curTime >= durTime) {
@@ -152,7 +109,7 @@ let phoneRender = (function () {
     }
 
     return {
-        init: function () {
+        init: function init() {
             $phone.css('display', 'block');
 
             //->PLAY BELL
@@ -164,12 +121,12 @@ let phoneRender = (function () {
             //->DETAIL TOUCH
             $detailTouch.singleTap(message);
         }
-    }
-})();
+    };
+}();
 
 /*--MESSAGE--*/
-let messageRender = (function () {
-    let $message = $('.message'),
+var messageRender = function () {
+    var $message = $('.message'),
         musicAudio = $('#musicAudio')[0],
         $wrapper = $message.find('.wrapper'),
         $messageList = $wrapper.find('li'),
@@ -177,12 +134,12 @@ let messageRender = (function () {
         $text = $keyboard.find('.text'),
         $submit = $keyboard.find('.submit');
 
-    let autoTimer = null,
+    var autoTimer = null,
         step = -1,
         initTranslateY = 0;
 
     function messageMove() {
-        let $cur = $messageList.eq(++step);
+        var $cur = $messageList.eq(++step);
         //->显示每一条消息
         $cur.css({
             transform: 'translateY(0)',
@@ -193,7 +150,7 @@ let messageRender = (function () {
         if (step === 2) {
             clearInterval(autoTimer);
             //->我们需要操作两个样式执行过渡动画,事件被执行两次(webkitTransitionEnd:有几个样式需要执行过渡动画,事件就会被触发执行几次)
-            let fn = function () {
+            var fn = function fn() {
                 $cur.off('webkitTransitionEnd', fn);
                 keyboard();
             };
@@ -211,7 +168,7 @@ let messageRender = (function () {
             clearInterval(autoTimer);
             musicAudio.pause();
             $(musicAudio).remove();
-            setTimeout(()=> {
+            setTimeout(function () {
                 $message.remove();
                 cubeRender.init();
             }, 2000);
@@ -224,18 +181,18 @@ let messageRender = (function () {
 
         //->显示文字：文字打印机
         //JQ中的ONE也是绑定事件,只不过只绑定一次而已,触发一次后自动把绑定的方法移除
-        $keyboard.one('webkitTransitionEnd', ()=> {
-            let str = '都学了啊，可我还是找不到好工作！',
+        $keyboard.one('webkitTransitionEnd', function () {
+            var str = '都学了啊，可我还是找不到好工作！',
                 textTimer = null,
                 n = -1;
-            textTimer = setInterval(()=> {
+            textTimer = setInterval(function () {
                 if (n >= str.length - 1) {
                     clearInterval(textTimer);
                     //->显示提交按钮
                     $submit.css('display', 'block');
                     return;
                 }
-                let textVal = $text.html();
+                var textVal = $text.html();
                 textVal += str[++n];
                 $text.html(textVal);
             }, 100);
@@ -253,14 +210,14 @@ let messageRender = (function () {
     }
 
     return {
-        init: function () {
+        init: function init() {
             $message.css('display', 'block');
             musicAudio.play();
             autoTimer = setInterval(messageMove, 1500);
             submitEvent();
         }
-    }
-})();
+    };
+}();
 
 /*--CUBE--*/
 //=>移动端设备中的浏览器或者是微信等,当手指滑动的时候都会有一些属于自己的默认行为(浏览器滑动有切换页卡的默认行为),为了保证我们的滑动能正常,我们需要阻止这些默认行为
@@ -268,8 +225,8 @@ $(document).on('touchstart touchmove touchend', function (ev) {
     ev.preventDefault();
 });
 
-let cubeRender = (function () {
-    let $cube = $('.cube'),
+var cubeRender = function () {
+    var $cube = $('.cube'),
         $box = $cube.children('ul');
 
     //->记录起始的旋转角度,每一次的滑动都是在上一次角度基础上继续旋转的
@@ -279,7 +236,7 @@ let cubeRender = (function () {
     });
 
     function start(ev) {
-        let point = ev.changedTouches[0];
+        var point = ev.changedTouches[0];
         $(this).attr({
             strX: point.pageX,
             strY: point.pageY,
@@ -290,8 +247,8 @@ let cubeRender = (function () {
     }
 
     function move(ev) {
-        let point = ev.changedTouches[0];
-        let changeX = point.pageX - $(this).attr('strX'),
+        var point = ev.changedTouches[0];
+        var changeX = point.pageX - $(this).attr('strX'),
             changeY = point.pageY - $(this).attr('strY');
         if (Math.abs(changeX) > 10 || Math.abs(changeY) > 10) {
             $(this).attr({
@@ -303,16 +260,16 @@ let cubeRender = (function () {
     }
 
     function end(ev) {
-        let isMove = $(this).attr('isMove');
+        var isMove = $(this).attr('isMove');
         if (isMove !== 'true') return;
 
-        let changeX = parseFloat($(this).attr('changeX')),
+        var changeX = parseFloat($(this).attr('changeX')),
             changeY = parseFloat($(this).attr('changeY')),
             rotateX = parseFloat($(this).attr('rotateX')),
             rotateY = parseFloat($(this).attr('rotateY'));
         rotateX = rotateX - changeY / 3;
         rotateY = rotateY + changeX / 3;
-        $(this).css('transform', `scale(0.6) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`);
+        $(this).css('transform', 'scale(0.6) rotateX(' + rotateX + 'deg) rotateY(' + rotateY + 'deg)');
         $box.attr({
             rotateX: rotateX,
             rotateY: rotateY
@@ -320,31 +277,27 @@ let cubeRender = (function () {
     }
 
     return {
-        init: function () {
+        init: function init() {
             $cube.css('display', 'block');
-            $box.on('touchstart', start)
-                .on('touchmove', move)
-                .on('touchend', end)
-                .find('li').tap(function () {
+            $box.on('touchstart', start).on('touchmove', move).on('touchend', end).find('li').tap(function () {
                 //->ZP中提供了一些快捷的移动操作方法:tap、singleTap、doubleTap、longTap、swipe、swipeLeft...
                 $cube.remove();
-                let index = $(this).index();
+                var index = $(this).index();
                 swiperRender.init(index);
             });
         }
-    }
-})();
+    };
+}();
 
 /*--SWIPER--*/
-let swiperRender = (function () {
-    let $swiperContainer = $('.swiper-container'),
+var swiperRender = function () {
+    var $swiperContainer = $('.swiper-container'),
         example = null,
         $makisu = $('.makisu');
 
     function change(ex) {
-        let index = ex.activeIndex,
+        var index = ex.activeIndex,
             slideAry = ex.slides;
-
         if (index === 0) {
             $makisu.makisu({
                 selector: 'dd',
@@ -362,13 +315,13 @@ let swiperRender = (function () {
         }
 
         //->给当前页设置ID,其余页面移除ID
-        $.each(slideAry, (n, item)=> {
-            item.id = n === index ? `page${index + 1}` : ``;
+        $.each(slideAry, function (n, item) {
+            item.id = n === index ? 'page' + (index + 1) : '';
         });
     }
 
     return {
-        init: function (index) {
+        init: function init(index) {
             index = index || 0;
             $swiperContainer.css('display', 'block');
 
@@ -380,10 +333,7 @@ let swiperRender = (function () {
             });
             example.slideTo(index, 0);
         }
-    }
-})();
+    };
+}();
 
-
-loadingRender.init(1);
-
-
+cubeRender.init(1);
